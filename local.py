e8d4f81901dd6d1b04800545818a439e3cbcbdf9
sXy98713 <842263538@qq.com>
2019-04-08 15:51:42 +0800

    运行在本地，接收客户端数据并转发给服务器remote

--- local.py
+++ local.py
@@ -4,17 +4,25 @@ from sxysocks import Connection
 from sxysocks import copy_to
 
 async def start_local(reader, writer):
-    local_server = Connection(reader, writer)
-    remote_reader, remote_writer = asyncio.open_connection('39.97.110.59',9713)
-    remote_server = Connection(remote_reader, remote_writer)
-    await asyncio.gather(
-        copy_to(local_server, remote_server, False),
-        copy_to(remote_server, local_server, True)
-    )
+    try:
+        
+        local_server = Connection(reader, writer)
+        
+        remote_reader, remote_writer = await asyncio.open_connection('39.97.110.59',8008)
+        remote_server = Connection(remote_reader, remote_writer)
+        task = [copy_to(local_server, remote_server, False), copy_to(remote_server, local_server, True)]
+        await asyncio.wait(task)
+    except Exception as e:
+        print(e)
+
+
 async def main():
     port = 8008
-    #print(port)
+    print(port)
     local_server = await asyncio.start_server(start_local, '127.0.0.1',port)
+    addr = local_server.sockets[0].getsockname()
+    print(f'Serving on {addr}')
+
     async with local_server:
         await local_server.serve_forever()
 
